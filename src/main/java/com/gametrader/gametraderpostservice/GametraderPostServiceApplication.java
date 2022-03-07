53f3f4b6c5d72595fdcf07fa0f2646b5f38ca3b4
ptrrk1337 <mymyry1999@gmail.com>
2022-03-07 18:59:08 +0100

    GAMETRADER-41 Initialized post service

---
+++ src/main/java/com/gametrader/gametraderpostservice/GametraderPostServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gametrader.gametraderpostservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GametraderPostServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GametraderPostServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gametrader/gametraderpostservice/GametraderPostServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gametrader.gametraderpostservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GametraderPostServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
